d9033bb30691908a45ade2bbb4a99e6dfb92566c
Matias Cabello <127139662+matthzc332@users.noreply.github.com>
2025-04-28 14:43:24 -0300

    clases de javascript hasta la 3

---
+++ javascript/modoStrict/02-ModoStrict.js
@@ -0,0 +1,14 @@
+"use strict";  //no permite funciones sin declarar
+
+
+let x = 10;
+console.log(x);
+
+miFuncion();
+
+function miFuncion(){
+    
+    y = 10;
+    console.log(y);
+}
+miFuncion();
