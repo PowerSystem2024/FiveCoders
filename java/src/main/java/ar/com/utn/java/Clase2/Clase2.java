fc89f9b02e2d6e9af2b3f269afe60097bf6fe121
Jonathan Suarez <jony35622@gmail.com>
2025-04-30 21:48:28 -0300

    Agrego carpeta java como un módulo para integrar todas las clases dentro y que puedan correr bien

---
+++ java/src/main/java/ar/com/utn/java/Clase2/Clase2.java
@@ -0,0 +1,7 @@
+package ar.com.utn.java.Clase2;
+
+public class Clase2 {
+    public static void main(String[] args) {
+        System.out.println("Esta es otra clase de ejemplo 2 a reemplazar");
+    }
+}
