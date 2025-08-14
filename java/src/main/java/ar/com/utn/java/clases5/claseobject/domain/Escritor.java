b0f64002e7c431698766a115cd94b3c5740dee64
JONATHAN SUAREZ <73504770+jonathan35622@users.noreply.github.com>
2025-08-13 22:30:37 -0300

    Delete java directory - restructurado dentro de tercer-semestre

--- java/src/main/java/ar/com/utn/java/clases5/claseobject/domain/Escritor.java
+++
@@ -1,4 +0,0 @@
-package ar.com.utn.java.clases5.claseobject.domain;
-
-public class Escritor {
-}
--- java/src/main/java/ar/com/utn/java/clases5/conversionobjetos/domain/Editor.java
+++
@@ -1,4 +0,0 @@
-package ar.com.utn.java.clases5.conversionobjetos.domain;
-
-public class Editor {
-}
