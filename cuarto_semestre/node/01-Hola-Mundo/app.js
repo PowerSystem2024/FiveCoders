635f958e91e8de70a7ec71ceaba1a7f2d894c29a
Jonathan Suarez <jony35622@gmail.com>
2025-09-01 18:38:24 -0300

    Subo clase 1-2-3 de Node - https://github.com/PowerSystem2024/FiveCoders/issues/79

---
+++ cuarto_semestre/node/01-Hola-Mundo/app.js
@@ -0,0 +1,7 @@
+const nombre = 'Fivecoders';
+console.log('Hola ' + nombre);
+
+const nombreEquipo = 'Equipo de Fivecoders';
+console.log('Hola ' + nombreEquipo);
+
+console.log('Hola mundo desde Node ');
